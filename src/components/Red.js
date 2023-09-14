b32df5cbff786b674185a8c96b8343e5eb02d535
soyeolesafm <soyelesafm@hotmail.com>
2023-09-13 22:09:38 -0500

    burger button

--- src/components/Red.js
+++ src/components/Red.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Hamburger from "./Hamburger";
 
 const Red = () => {
     return (
-        <div className=" bg-red-500 h-screen " id="red" ></div>
+        <div className=" bg-red-500 h-screen " id="red" >
+            <Hamburger />
+        </div>
     )
 }
 
