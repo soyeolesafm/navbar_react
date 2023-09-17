30cef5aef68662d9d74df83388774e9d25e76334
soyeolesafm <soyelesafm@hotmail.com>
2023-09-17 10:24:12 -0500

    responsivness at first

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     <nav className='flex justify-between items-center h-24 m-0 p-0 mx-auto px-4 text-white bg-slate-900 fixed w-screen ' >
         
         <h1 className= { isOpen? 'hidden' : 'w-full text-3xl font-bold text-[#00df9a] ml-24 ' } > LOGO </h1>
-        <ul className='flex justify-between items-center space-x-2 mr-24 ' >
+        <ul className='hidden md:flex justify-between items-center space-x-2 mr-24 ' >
             <li className='p-2 text-lg ' > 
                 <a href='#red' > red </a>
              </li>
@@ -28,7 +28,7 @@ const Navbar = () => {
 
         {/* burger button */}
         <div 
-        className="  fixed m-auto top-6 right-6 cursor-pointer " 
+        className="fixed m-auto top-9 right-10 cursor-pointer md:hidden " 
         onClick={ openMenu }
          >
             <div className=  { isOpen ? "rotate-45 ease-in-out duration-300 translate-y-1.5 translate-x-1 bg-slate-50 w-6 h-0.5 rounded-sm m-1 " : " ease-in-out duration-300 bg-slate-50 w-6 h-0.5 rounded-sm m-1 " } ></div>
