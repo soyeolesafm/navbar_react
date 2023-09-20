62f9708e1a353e3152c88247c07a1331f3bc03c9
soyeolesafm <soyelesafm@hotmail.com>
2023-09-19 23:08:33 -0500

    logo fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
     <nav className='flex justify-between items-center h-24 m-0 p-0 mx-auto px-4 text-white bg-slate-900 fixed w-screen ' >
         
-        <h1 className= ' z-30 w-full text-3xl font-bold text-[#00df9a] ml-24 '  > LOGO </h1>
+        <h1 className= ' z-30 w-full text-3xl font-bold text-[#00df9a] ml-10 md:ml-24 '  > LOGO </h1>
         <ul className='hidden md:flex justify-between items-center space-x-2 mr-24 ' >
             <li className='p-2 text-lg ' > 
                 <a href='#red' > red </a>
